fa8eef73dba7834cc3fe65f2e74ec7dfa46469c1
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-16 22:28:42 +0200

    ok
    ok

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace T05Ejercicio8
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
